e3bcca10be64a4b3c6acdacc7c5567e8e29954f1
Ol-create <oluola96@gmail.com>
2022-05-11 15:43:35 -0600

    feat: create and export addBook action

--- src/redux/books.js
+++ src/redux/books.js
@@ -1,3 +1,8 @@
 const ADDBOOK = 'bookstore/books/ADDBOOK';
 const REMOVEBOOK = 'bookstore/books/REMOVEBOOK';
 const initialState = [];
+
+export const addBook = (title) => ({
+	type: ADDBOOK,
+	book: { title },
+  });
