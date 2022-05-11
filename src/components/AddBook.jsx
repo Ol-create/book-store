71f03b266d14781fa7d2506915be473408206672
Ol-create <oluola96@gmail.com>
2022-05-11 16:43:56 -0600

    feat: add dispatch addBook function

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const AddBook = () => (
-  <form className="addBook">
-    <h2>ADD NEW BOOK</h2>
-    <input type="text" name="title" placeholder="Book Title" />
-    <input type="text" name="author" placeholder="Book Author" />
-    <button className="addBtn" type="submit">Add Books</button>
-  </form>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
 
-export default AddBook;
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (!e.target.elements[1].value) {
+      dispatch(addBook(e.target.elements[0].value, 'Anonymous'));
+    } else {
+      dispatch(addBook(e.target.elements[0].value, e.target.elements[1].value));
+    }
+  };
+
+  return (
+    <form className="addBook" onSubmit={submitHandler}>
+      <h2>ADD NEW BOOK</h2>
+      <input type="text" name="title" placeholder="Book Title" required />
+      <input type="text" name="author" placeholder="Book Author" />
+      <button className="addBtn" type="submit">Add Books</button>
+    </form>
+  );
+};
