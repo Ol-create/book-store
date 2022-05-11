73419d6f4a4ca7737d1092f324b168672d86c29c
Ol-create <oluola96@gmail.com>
2022-05-11 16:37:38 -0600

    feat: add progress text align to center

---
+++ src/redux/books/books.js
@@ -0,0 +1,26 @@
+const ADDBOOK = 'bookstore/books/ADDBOOK';
+const REMOVEBOOK = 'bookstore/books/REMOVEBOOK';
+const initialState = [];
+
+export const addBook = (title) => ({
+  type: ADDBOOK,
+  book: { title },
+});
+
+export const removeBook = (id) => ({
+  type: REMOVEBOOK,
+  book: id,
+});
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADDBOOK:
+      return [...state, action.book];
+    case REMOVEBOOK:
+      return state.filter((book, index) => index !== action.book);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
