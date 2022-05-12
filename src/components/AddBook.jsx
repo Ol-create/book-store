5fcba63ade70f1aafe7463dadcd10f23cdeb037e
Ol-create <oluola96@gmail.com>
2022-05-12 10:10:19 -0600

    feat: useSelector to compare state

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -24,3 +24,5 @@ const AddBook = () => {
     </form>
   );
 };
+
+export default AddBook;
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookElement from './SingleBook';
 import AddBook from './AddBook';
 
-const Books = () => (
-  <div>
-    <ul className="bookList">
-      <BookElement title="Book Title" author="Author Name" />
-    </ul>
-    <AddBook />
-  </div>
-);
+const Books = () => {
+  const bookList = useSelector((store) => store.books);
+  console.log(bookList);
+  return (
+    <div>
+      <ul className="bookList">
+        {bookList.map((book) => (
+          <BookElement key={book.id} title={book.title} author={book.author} id={book.id} />
+        ))}
+      </ul>
+      <AddBook />
+    </div>
+  );
+};
 
 export default Books;
