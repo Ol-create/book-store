0b82380b90e5a078b3ac199d34192809751bbb59
Oluyemi Paul <oluola96@gmail.com>
2022-06-04 21:10:59 -0600

    Update books.js

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -7,15 +7,15 @@ const ADDBOOK = 'bookstore/books/ADDBOOK';
 const REMOVEBOOK = 'bookstore/books/REMOVEBOOK';
 const initialState = [];
 
-export const addBook = (title) => ({
-  type: ADDBOOK,
-  book: { title },
-});
+// export const addBook = (title) => ({
+//   type: ADDBOOK,
+//   book: { title },
+// });
 
-export const removeBook = (id) => ({
-  type: REMOVEBOOK,
-  book: id,
-});
+// export const removeBook = (id) => ({
+//   type: REMOVEBOOK,
+//   book: id,
+// });
 
 const apiAddBook = async (id, title, author) => {
   const add = await fetch(api, {
